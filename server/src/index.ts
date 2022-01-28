1df9dd111712cd8f27dc0ca891bddc65a96b8a26
Kubi <kubilayercikti1@gmail.com>
2022-01-28 19:51:56 +0100

    route for refresh access token

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,17 @@ import UserResolver from "./resolvers/user";
 import { createConnection } from "typeorm";
 import cors from "cors";
 import "dotenv-safe/config";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken } from "./utils/auth";
 
 (async () => {
   const app = express();
 
   app.set("trust proxy", 1);
+
+  app.use(cookieParser());
 
   app.use(
     cors({
@@ -18,6 +24,30 @@ import "dotenv-safe/config";
       credentials: true,
     })
   );
+
+  app.post("/refresh_token", async (req, res) => {
+    const token: string = req.cookies.jid;
+
+    if (!token) {
+      res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (error) {
+      console.error(error);
+      res.send({ ok: false, accessToken: "" });
+    }
+
+    const user = await User.findOne(payload.userId);
+
+    if (!user) {
+      res.send({ ok: false, accessToken: "" });
+    }
+
+    res.send({ ok: true, accesToken: createAccessToken(user) });
+  });
 
   await createConnection();
 
